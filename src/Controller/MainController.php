711bf960a905da0caa610f281e731c97c1d8c439
Jonathanbtq <botquin.jonathan@yahoo.fr>
2023-04-29 15:44:55 +0200

    mac 24 index surtout et main controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostsRepository;
 use App\Repository\PublicationsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,10 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
-    public function index(PublicationsRepository $publiRepo): Response
+    public function index(PublicationsRepository $publiRepo, PostsRepository $postRepo): Response
     {
         return $this->render('main/index.html.twig', [
-            'publications' => $publiRepo->findAll()
+            'publications' => $publiRepo->findAll(),
+            'posts' => $postRepo->findAll(),
         ]);
     }
 }
